6bb27e0a986a76cc0705c3b3a139c6a1dbf6a059
malits <maxlitster@gmail.com>
2020-05-14 14:19:05 -0400

    requirements and some setup stuff

---
+++ startup.sh
@@ -0,0 +1,6 @@
+jupyter contrib nbextension install --user
+
+jupyter nbextensions_configurator enable --user
+
+jupyter nbextension enable collapsible_headings/main
+jupyter nbextension enable toc2/main
